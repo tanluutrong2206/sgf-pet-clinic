d58c4de7e704078faa5a99e9e4a9705094ca4b00
Tấn Lưu Trọng <tanluutrong2206@gmail.com>
2020-08-30 11:27:10 +0700

    refactoring to multimodule mvn project. Closes #1

--- sgf-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sgfpetclinic/SgfPetClinicTest.java
+++ sgf-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sgfpetclinic/SgfPetClinicTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class SgfPetClinicTest {
+    @Test
+    void contextLoads() {
+    }
 }
